bccb340a125505b84f45e7b0dcd8f0c55a81513d
jinhobyeong <jhb_0921@naver.com>
2022-01-08 23:34:23 +0900

    fill add

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("controls__color");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
 
-console.log(colors);
+const INITAIL_COLOR = "#2c2c2c";
 
 canvas.width = 700;
 canvas.height = 700;
 
-ctx.strokeStyle = "##2c2c2c";
+ctx.fillStyle = "white";
+ctx.fillRect(0,0,canvas.width, canvas.height);
+ctx.strokeStyle = INITAIL_COLOR;
+ctx.fillStyle = INITAIL_COLOR;
+
 ctx.lineWidth = 2.5; 
 
 let painting = false;
@@ -40,6 +46,30 @@ function onMouseMove(event){
 
 function handleColor(event){
    ctx.strokeStyle = event.target.style.backgroundColor;
+   ctx.fillStyle = ctx.strokeStyle;
+
+}
+
+function handleRange(event){
+    ctx.lineWidth = (event.target.value);
+ }
+
+ function changeMode(){
+     if(mode.innerText == "PAINT"){
+         mode.innerText = "FILL";
+         
+        //mode change code
+     }
+     else{
+         mode.innerText = "PAINT";
+     }
+
+ }
+
+function handleCanvasClick(){
+    if(mode.innerText == "FILL"){
+        ctx.fillRect(0,0,canvas.width, canvas.height);
+    }
 }
 
 if(canvas){
@@ -47,6 +77,16 @@ if(canvas){
     canvas.addEventListener("mousedown", startPainting); //click
     canvas.addEventListener("mouseup", stopPainting); //click off
     canvas.addEventListener("mouseleave", stopPainting); //이건 그림 그릴수 있느 ㄴ공간을 나갔을때 ? 
+    canvas.addEventListener("click", handleCanvasClick);
 }
 
-Array.from(colors).forEach(color => color.addEventListener("click", handleColor));
+Array.from(colors).forEach(color => 
+    color.addEventListener("click", handleColor)
+);
+
+if(range){
+    range.addEventListener("input", handleRange);
+}
+if(mode){
+    mode.addEventListener("click", changeMode);
+}
